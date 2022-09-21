03c88a6bfcc7ce84b279ebc404725fcd1c8b14d0
Margarita S <runlet@inbox.ru>
2022-09-21 11:34:19 +0300

    Second commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿string[] array1 = new string[3] { "hello", "2", "world" };
+string[] array2 = new string[array1.Length];
+void GetSecondArray(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if (array1[i].Length <= 3)
+        {
+            array2[count] = array1[i];
+            count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+GetSecondArray(array1, array2);
+PrintArray(array2);
+
+
+
+
+
+
